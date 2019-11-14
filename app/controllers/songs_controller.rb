a6941238d4c9293517add28b6cc7faefff5647e3
Trish Ogen <patriciaogen@gmail.com>
2019-11-14 02:16:28 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -52,7 +52,7 @@ class SongsController < ApplicationController
         redirect_to artist_songs_path(artist), alert: "Song not found." if @song.nil?
       end
     else
-      @song = song.find(params[:id])
+      @song = Song.find(params[:id])
     end
   end
 
