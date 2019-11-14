fc9f5182fac51b34f8eb891ed3328c5f3add643c
Trish Ogen <patriciaogen@gmail.com>
2019-11-14 02:13:27 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,11 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+      redirect_to artists_path, alert: "Artist not found."
+    else
+      @song = Song.new(artist_id: params[:artist_id])
+    end
   end
 
   def create
@@ -67,4 +71,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
-
